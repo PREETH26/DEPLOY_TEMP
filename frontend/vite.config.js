c3f1b58b6a6536a9bcce04f319a76bc0ec6d573b
Preetham Praveen <preethampraveencode26@gmail.com>
2025-03-30 14:54:18 +0530

    preview

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+  base: '/',
   assetsInclude: ["**/pdf.worker.min.js", "**/pdf.worker.js"], // Treat these files as assets
     server: {
         watch: {
